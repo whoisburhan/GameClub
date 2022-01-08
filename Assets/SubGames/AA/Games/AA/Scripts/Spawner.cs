aca0229ed5f478d0efb3a80adccede9769574cf6
BurhanUlka <whoisburhan@gmail.com>
2022-01-08 07:56:08 -0800

    Create Spawner.cs

---
+++ Assets/SubGames/AA/Games/AA/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GS.AA
+{
+    public class Spawner : MonoBehaviour
+    {
+        [HideInInspector]
+        public int SpawnedBallCounter = 0;
+
+        public void SpawnBall()
+        {
+            if(SpawnedBallCounter > 0)
+            {
+                Destroy(Instantiate(GameManager.Instance.rippleEffect, this.transform.position, this.transform.rotation), 2f);
+                GameObject go = Instantiate(GameManager.Instance.BallPrefab, this.transform.position, this.transform.rotation);
+
+                Ball ball = go.GetComponent<Ball>();
+                if(ball != null)
+                {
+                    ball.BallNo.text = SpawnedBallCounter.ToString();
+                    ball.spawnFrom = this;
+                    ball.BallNo.transform.rotation = Quaternion.identity;
+                }
+                SpawnedBallCounter--;
+            }
+        }
+    }
+}
