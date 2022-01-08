c20e619e4ed152193531d3b0c3fb809f2613567b
BurhanUlka <whoisburhan@gmail.com>
2022-01-08 07:56:05 -0800

    Create Sarah.cs

---
+++ Assets/SubGames/AA/Games/AA/Scripts/Sarah.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GS.AA
+{
+    public class Sarah : MonoBehaviour
+    {
+        public Text DialogueText;
+        [SerializeField] private GameObject sarahsBackground;
+
+        private void OnEnable()
+        {
+            sarahsBackground.SetActive(true);
+            GameManager.Instance.IsPlay = false;
+            if (GameManager.Instance.requestForAppReview)
+            {
+#if UNITY_ANDROID || UNITY_IOS
+               // StartCoroutine(InAppReviewManager.Instance.RequestReview());
+#endif
+            }
+        }
+
+        private void OnDisable()
+        {
+            sarahsBackground.SetActive(false);
+            GameManager.Instance.IsPlay = true;
+        }
+
+        public void SetDialogue(string _dialogue = "")
+        {
+            DialogueText.text = _dialogue;
+            GameManager.Instance.IsPlay = true;
+        }
+
+        public void DeactivateSarah()
+        {
+            this.gameObject.SetActive(false);
+           
+
+            if (GameManager.Instance.requestForAppReview)
+            {
+                GameManager.Instance.requestForAppReview = false;
+            }
+            else
+            {
+#if UNITY_ANDROID || UNITY_IOS
+                //AdmobAds.instance.reqBannerAd();
+#endif
+            }
+        }
+    }
+}
